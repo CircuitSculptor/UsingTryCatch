bf110fce9c6c3299e728a5269367df0aa4420909
Bartlomiej Drapinski <g00428194@atu.ie>
2024-10-22 18:00:13 +0100

    Initial Setup

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -1,6 +1,19 @@
 package ie.atu.week6;
+import java.util.Scanner;
+
 public class UsingTryCatch {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Please enter a number between 0 - 9: ");
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        int Num = Integer.parseInt(input);
+
+        if (Num >= 0 && Num <= 9) {
+            System.out.println("You entered " + Num);
+        }
+        else {
+            System.out.println("Not a valid number");
+        }
     }
 }
